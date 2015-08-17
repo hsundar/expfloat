72a403a21e635900da8bd384d6969c6cacc53acb
Hari Sundar <hari@cs.utah.edu>
2015-08-17 13:56:42 -0600

    added mult and dot product

--- include/expansion_math.h
+++ include/expansion_math.h
@@ -5,6 +5,11 @@
 #ifndef EXPFLOAT_EXPANSION_MATH_H
 #define EXPFLOAT_EXPANSION_MATH_H
 
+#define S 16
+
+// @brief computes the sum of two single precision floating point values and computes
+// their double precision sum in the expansion form
+
 inline void
 fast_two_sum(float a, float b, float& x, float& y) {
     float v;
@@ -20,26 +25,91 @@ fast_two_sum(float a, float b, float& x, float& y) {
 }
 
 inline void
-two_sum(float a, float b, float* x, float* y) {
+two_sum(float a, float b, float& x, float& y) {
     float av, bv, ar, br;
 
-    *x = a+b;
+    x = a+b;
 
-    bv = *x - a;
-    av = *x - bv;
+    bv = x - a;
+    av = x - bv;
 
     br = b - bv;
     ar = a - av;
 
-    *y = ar+ br;
+    y = ar+ br;
 }
 
 inline void
 grow_expansion (float& e1, float& e2, float b) {
     float q, h;
 
-    fast_two_sum(b, e2, q, h);
-    fast_two_sum(q, e1, e1, e2);
+    two_sum(b, e2, q, h);
+    two_sum(q, e1, e1, e2);
 }
+
+inline void
+fast_expansion_sum (float& e1, float& e2, float f1, float f2) {
+    float q,h;
+
+    two_sum(f2, e2, q, h);
+    if (e1 < f1) { // want branch to fail
+        two_sum(q,e1,q,h);
+        two_sum(q,f1,e1,e2);
+    } else {
+        two_sum(q,f1,q,h);
+        two_sum(q,e1,e1,e2);
+
+    }
+
+}
+
+inline void
+split (float a, float& a_hi, float& a_lo) {
+    float c, ab;
+
+    c = ((1 << S) + 1) * a;
+    ab = c - a;
+    a_hi = c - ab;
+    a_lo = a - a_hi;
+}
+
+inline void
+two_product (float a, float b, float&x, float& y) {
+    float a_hi, a_lo, b_hi, b_lo, err1, err2;
+
+    x = a*b;
+
+    split (a, a_hi, a_lo);
+    split (b, b_hi, b_lo);
+
+    err1 = x - (a_hi * b_hi);
+    err2 = err1 - (a_lo * b_hi);
+    err1 = err2 - (a_hi * b_lo);
+
+    y = (a_lo * b_lo) - err1;
+}
+
+// todo Add code for dot product, matvec, dgemm, rk4
+
+inline void exp_dot (float* a, float* b, unsigned int n, float& r1, float& r2) {
+    float x=0.0, y=0.0;
+    r1=0.0; r2=0.0;
+    for (int i = 0; i < n; ++i) {
+        // two_product(a[i], b[i], x, y);
+        // fast_expansion_sum(r1, r2, x, y);
+        grow_expansion(r1, r2, a[i]*b[i]);
+    }
+}
+
+template <typename T>
+inline T dot (T* a, T* b, int n) {
+    T res = 0.0;
+    for (int i = 0; i < n; ++i) {
+        res += a[i]*b[i];
+    }
+    return res;
+}
+
+
 
 #endif //EXPFLOAT_EXPANSION_MATH_H
--- src/main.cpp
+++ src/main.cpp
@@ -9,63 +9,118 @@
 #define CPU_SPEED 2.2E+09
 
 int main() {
-    int i;
-    float a, b, x, y;
-    double da, db, dz;
-    float e1 = 0.0, e2 = 0.0, sum = 0.0;
-    double dsum = 0.0;
-    float *arr = (float *) malloc(N * sizeof(float));
-    double *darr = (double *) malloc(N * sizeof(double));
+  int i;
+  float a, b, x, y;
+  double da, db, dz;
+  float e1 = 0.0, e2 = 0.0, sum = 0.0;
+  double dsum = 0.0;
+  float *arr1 = (float *) malloc(N * sizeof(float));
+  float *arr2 = (float *) malloc(N * sizeof(float));
+  double *darr1 = (double *) malloc(N * sizeof(double));
+  double *darr2 = (double *) malloc(N * sizeof(double));
 
-    // unsigned long long t1, t2, t3, t4;
-    double t1, t2, t3, t4;
+  printf("\n=========== a+b ===============\n");
 
-    srand(time(NULL));
+  // unsigned long long t1, t2, t3, t4;
+  double t1, t2, t3, t4;
 
-    a = rand();
+  srand(time(NULL));
+
+  a = rand();
+  a /= RAND_MAX;
+  b = rand();
+  b /= RAND_MAX;
+
+  fast_two_sum(a, b, x, y);
+
+  printf("a: %.8f, b: %.8f, x: %.8f, y: %.8e \n", a, b, x, y);
+
+  da = a;
+  db = b;
+
+  dz = da + db;
+
+  printf("dz: %.14g, delta: %.9g\n", dz, dz - x);
+
+  printf("\n=========== sum(a) ==============\n");
+
+  for (i = 0; i < N; ++i) {
+    da = rand();
+    a = da;
     a /= RAND_MAX;
-    b = rand();
-    b /= RAND_MAX;
+    da /= RAND_MAX;
+    arr1[i] = a;
+    darr1[i] = da;
 
-    two_sum(a, b, &x, &y);
+    da = rand();
+    a = da;
+    a /= RAND_MAX;
+    da /= RAND_MAX;
+    arr2[i] = a;
+    darr2[i] = da;
+  }
 
-    printf("a: %.8f, b: %.8f, x: %.8f, y: %.8e \n", a, b, x, y);
-
-    da = a;
-    db = b;
-
-    dz = da + db;
-
-    printf("dz: %.14g, delta: %.9g\n", dz, dz - x);
-
-    printf("\n============================\n");
-
-    for (i = 0; i < N; ++i) {
-        da = rand();
-        a = da;
-        a /= RAND_MAX;
-        da /= RAND_MAX;
-        arr[i] = a;
-        darr[i] = da;
-    }
-
-    t1 = rdtsc();
-    for (i = 0; i < N; ++i)
-        sum = sum + arr[i];
-    t2 = rdtsc();
-    for (i = 0; i < N; ++i)
-        dsum = dsum + darr[i];
-    t3 = rdtsc();
-    for (i = 0; i < N; ++i)
-        grow_expansion(e1, e2, arr[i]);
-    t4 = rdtsc();
+  t1 = rdtsc();
+  for (i = 0; i < N; ++i)
+    sum = sum + arr1[i];
+  t2 = rdtsc();
+  for (i = 0; i < N; ++i)
+    dsum = dsum + darr1[i];
+  t3 = rdtsc();
+  for (i = 0; i < N; ++i)
+    grow_expansion(e1, e2, arr1[i]);
+  t4 = rdtsc();
 
 
-    printf("sum: %.8f, dsum: %.15g \n", sum, dsum);
-    printf("expansion: %.8f, %.8e\n", e1, e2);
-    printf("delta: %.9g\n", dsum - e1);
+  printf("sum: %.8f, dsum: %.15g \n", sum, dsum);
+  printf("expansion: %.8f, %.8e\n", e1, e2);
+  printf("delta: %.9g\n", dsum - e1);
 
-    printf("times: %g, %g, %g\n", (t2 - t1) / CPU_SPEED, (t3 - t2) / CPU_SPEED, (t4 - t3) / CPU_SPEED);
+  printf("times: %g, %g, %g\n", (t2 - t1) / CPU_SPEED, (t3 - t2) / CPU_SPEED, (t4 - t3) / CPU_SPEED);
 
-    return 0;
+  printf("\n=========== a*b ===============\n");
+
+  a = rand();
+  a /= RAND_MAX;
+  b = rand();
+  b /= RAND_MAX;
+
+  two_product(a, b, x, y);
+
+  printf("a: %.8f, b: %.8f, x: %.8f, y: %.8e \n", a, b, x, y);
+
+  da = a;
+  db = b;
+
+  dz = da * db;
+
+  printf("dz: %.14g, delta: %.9g\n", dz, dz - x);
+
+  printf("\n========== (a,b) ==============\n");
+
+  t1 = rdtsc();
+  sum = dot(arr1, arr2, N);
+  t2 = rdtsc();
+  dsum = dot(darr1, darr2, N);
+  t3 = rdtsc();
+  exp_dot(arr1,arr2,N,e1,e2);
+  t4 = rdtsc();
+
+
+  printf("sum: %.8f, dsum: %.15g \n", sum, dsum);
+  printf("expansion: %.8f, %.8e\n", e1, e2);
+  printf("delta: %.9g\n", dsum - e1);
+
+  printf("times: %g, %g, %g\n", (t2 - t1) / CPU_SPEED, (t3 - t2) / CPU_SPEED, (t4 - t3) / CPU_SPEED);
+
+
+  printf("\n============================\n");
+
+  free(arr1);
+  free(arr2);
+  free(darr1);
+  free(darr2);
+
+  return 0;
 }
+
