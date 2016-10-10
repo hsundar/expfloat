aa9a4054995be39bdcba90a3aa5fb168cea3119c
Hari Sundar <hari@cs.utah.edu>
2016-10-09 20:31:16 -0600

    intermediate

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,44 @@
 #include <test_apps.h>
 #include <iostream>
 
-#define N 100000000
+#include <softfloat.h>
+#include <quadmath.h>
+#include <math.h>
+
+#include <inttypes.h>
+
+
+#define N 1000000
 #define CPU_SPEED 2.2E+09
+
+typedef unsigned __int128 uint128_t;
+typedef unsigned long uint64;
+
+
+/*      UINT64_MAX 18446744073709551615ULL */
+#define P10_UINT64 10000000000000000000ULL   /* 19 zeroes */
+#define E10_UINT64 19
+
+#define STRINGIZER(x)   # x
+#define TO_STRING(x)    STRINGIZER(x)
+
+static int print_u128_u(uint128_t u128)
+{
+    int rc;
+    if (u128 > UINT64_MAX)
+    {
+        uint128_t leading  = u128 / P10_UINT64;
+        uint64_t  trailing = u128 % P10_UINT64;
+        rc = print_u128_u(leading);
+        rc += printf("%." TO_STRING(E10_UINT64) PRIu64, trailing);
+    }
+    else
+    {
+        uint64_t u64 = u128;
+        rc = printf("%" PRIu64, u64);
+    }
+    return rc;
+}
 
 int main() {
   int i;
@@ -116,6 +152,7 @@ int main() {
   free(darr1);
   free(darr2);
 
+  /*
   printf("\n=========== Runge-Kutta ============\n");
 
   unsigned int n = 10000, r = 9;
@@ -149,8 +186,6 @@ int main() {
   std::cout << "time for rk4_float:  " << (t3 - t2) / CPU_SPEED << "s" << std::endl;
   std::cout << "time for rk4_exp:    " << (t4 - t3) / CPU_SPEED << "s" << std::endl;
 
-  /* Compute norms */
-
 
 
   delete[] q;
@@ -159,6 +194,89 @@ int main() {
   delete[] fq;
   delete[] fqrhs;
   delete[] fqres;
+
+  printf("\n============================\n");
+*/
+
+
+  //! @hari - 8 Oct 2016 - for Fall NSF Large 2016.
+  printf("\n=========== H-float ============\n");
+
+  float16_t    half_a;	// 16-bit half-precision binary format
+  float32_t    sing_a;  // 32-bit single-precision binary format
+  float64_t    doub_a;	// 64-bit double-precision binary format
+  extFloat80_t extf_a;  // 80-bit double-extended-precision binary format (old Intel or Motorola format)
+  float128_t 	 quad_a;  // 128-bit quadruple-precision binary format
+
+  unsigned long n = 1000000;
+  __float128  *sin2pi_q = new __float128[n];
+
+  char buf[128];
+  int width = 46;   
+
+  // sin2pi_q[0] = sinq(0.0q);
+  for (int i=0; i<n; ++i) {
+    sin2pi_q[i] = sinq(M_PIq * 2 * i / n / 1000);
+    // quadmath_snprintf (buf, sizeof(buf), "%+-.80Qe", hello[i]);
+    // printf("%s\n", buf);
+  }
+
+  // 128 bits hex - 0x ff ff ff ff ff ff ff ff 
+  // loop through ... 
+  unsigned long      cnts[12] = {1};
+  unsigned __int128  mask[12]; /* = {0x000000000000000000000000000003FF, //  10
+                                     0x000000000000000000000000000FFC00, //  20
+                                     0x0000000000000000000000003FF00000, //  30
+                                     0x0000000000000000000000FFC0000000, //  40
+                                     0x00000000000000000003FF0000000000, //  50
+                                     0x00000000000000000FFC000000000000, //  60
+                                     0x000000000000003FF000000000000000, //  70
+                                     0x000000000000FFC00000000000000000, //  80
+                                     0x0000000003FF00000000000000000000, //  90
+                                     0x0000000FFC0000000000000000000000, // 100
+                                     0x00003FF0000000000000000000000000, // 110
+                                     0x0000C000000000000000000000000000, // 112
+                                    }; */
+
+  unsigned __int128  bits;
+  unsigned __int128  base[12];
+
+  bits = *((__int128 *)(sin2pi_q));
+  printf("Masks ...\n");
+  for (int j=0; j<11; ++j) {
+    mask[j] = 0x03FFu;
+    mask[j] = mask[j] << j*10; 
+    base[j] = bits & mask[j];
+
+    // printf ("%d: mask = ", j);
+    // print_u128_u(mask[j]);
+    // printf("\n");
+    printf("%d:   \t  %016" PRIx64 "%016" PRIx64 "\n",j,(uint64)(mask[j]>>64),(uint64)mask[j]);
+
+  }
+  mask[11] = 0x03u;
+  mask[11] = mask[11] << 110; 
+  base[11] = bits & mask[11];
+  printf("%d: \t  %016" PRIx64 "%016" PRIx64 "\n",11,(uint64)(mask[11]>>64),(uint64)mask[11]);
+
+  quadmath_snprintf (buf, sizeof(buf), "%+-.32Qe", sin2pi_q[1] - sin2pi_q[0]);
+  printf("sin(2 pi x) -- %s\n", buf);
+  quadmath_snprintf (buf, sizeof(buf), "%+-.32Qe", sin2pi_q[2] - sin2pi_q[1]);
+  printf("sin(2 pi x) -- %s\n", buf);
+  quadmath_snprintf (buf, sizeof(buf), "%+-.32Qe", sin2pi_q[3] - sin2pi_q[2]);
+  printf("sin(2 pi x) -- %s\n", buf);
+  quadmath_snprintf (buf, sizeof(buf), "%+-.32Qe", sin2pi_q[4] - sin2pi_q[3]);
+  printf("sin(2 pi x) -- %s\n", buf);
+  quadmath_snprintf (buf, sizeof(buf), "%+-.32Qe", sin2pi_q[5] - sin2pi_q[4]);
+  printf("sin(2 pi x) -- %s\n", buf);
+
+  /* loop through
+     for (int i=1; i<n; ++i) {
+    cnts[0]++;
+  }
+  */
+
+  delete [] sin2pi_q;
 
   printf("\n============================\n");
 
